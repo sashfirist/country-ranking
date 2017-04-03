6d8f18580b690c2d3600098753a6ca577d2e29cb
Oleksandr Navka <Oleksandr.Navka@harmonicinc.com>
2017-04-03 18:25:41 +0300

    Initial commit

---
+++ src/main/java/ua/com/codespace/CountryRankingApplication.java
@@ -0,0 +1,12 @@
+package ua.com.codespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryRankingApplication.class, args);
+	}
+}
