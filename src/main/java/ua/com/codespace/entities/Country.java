33b47c4ae3042ea66cc88ba55ccd2ae3d4ce0510
kernel32 <sergii.rubinov@gmail.com>
2017-04-26 03:28:36 +0300

    copied project structure

--- src/main/java/ua/com/codespace/entities/Country.java
+++ src/main/java/ua/com/codespace/entities/Country.java
@@ -1,16 +1,30 @@
 package ua.com.codespace.entities;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by D10020 on 12.04.2017.
- */
+@Entity
+@Table(name = "country")
 public class Country {
 
-    String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-    public Country(String name) {
-        this.name = name;
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "country_id")
+    private List<CountryDetails> countryDetailsList = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -21,10 +35,40 @@ public class Country {
         this.name = name;
     }
 
+    public List<CountryDetails> getCountryDetailsList() {
+        return countryDetailsList;
+    }
+
+    public void setCountryDetailsList(List<CountryDetails> countryDetailsList) {
+        this.countryDetailsList = countryDetailsList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Country)) return false;
+
+        Country country = (Country) o;
+
+        if (getId() != country.getId()) return false;
+        if (getName() != null ? !getName().equals(country.getName()) : country.getName() != null) return false;
+        return getCountryDetailsList() != null ? getCountryDetailsList().equals(country.getCountryDetailsList()) : country.getCountryDetailsList() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (getId() ^ (getId() >>> 32));
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getCountryDetailsList() != null ? getCountryDetailsList().hashCode() : 0);
+        return result;
+    }
+
     @Override
     public String toString() {
         return "Country{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", countryDetailsList=" + countryDetailsList +
                 '}';
     }
 }
---
+++ src/main/java/ua/com/codespace/entities/CountryDetails.java
@@ -0,0 +1,91 @@
+package ua.com.codespace.entities;
+
+import ua.com.codespace.configuration.InformationType;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "country_details")
+public class CountryDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "information")
+    @Enumerated(EnumType.STRING)
+    private InformationType information;
+
+    @Column(name = "value")
+    private double value;
+
+    @Column(name = "year")
+    private int year;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public InformationType getInformation() {
+        return information;
+    }
+
+    public void setInformation(InformationType information) {
+        this.information = information;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CountryDetails)) return false;
+
+        CountryDetails that = (CountryDetails) o;
+
+        if (getId() != that.getId()) return false;
+        if (Double.compare(that.getValue(), getValue()) != 0) return false;
+        if (getYear() != that.getYear()) return false;
+        return getInformation() != null ? getInformation().equals(that.getInformation()) : that.getInformation() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = (int) (getId() ^ (getId() >>> 32));
+        result = 31 * result + (getInformation() != null ? getInformation().hashCode() : 0);
+        temp = Double.doubleToLongBits(getValue());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + getYear();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "CountryDetails{" +
+                "id=" + id +
+                ", information='" + information + '\'' +
+                ", value=" + value +
+                ", year=" + year +
+                '}';
+    }
+}
