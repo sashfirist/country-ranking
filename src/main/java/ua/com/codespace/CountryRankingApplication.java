56b81db8651bce5e2d64e5f62c42b688024bbb52
kernel32 <sergii.rubinov@gmail.com>
2017-04-26 03:28:48 +0300

    copied project structure

--- src/main/java/ua/com/codespace/CountryRankingApplication.java
+++ src/main/java/ua/com/codespace/CountryRankingApplication.java
@@ -1,9 +1,14 @@
 package ua.com.codespace;
 
-import org.springframework.boot.SpringApplication;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ua.com.codespace.entities.Country;
-import ua.com.codespace.entities.Ranking;
+import ua.com.codespace.entities.CountryDetails;
+import ua.com.codespace.configuration.InformationType;
 
 import java.util.*;
 
@@ -11,26 +16,35 @@ import java.util.*;
 public class CountryRankingApplication {
 
 	public static void main(String[] args) {
-		/*SpringApplication.run(CountryRankingApplication.class, args);*/
-		List<Ranking> rankingList = getRankingList();
-		System.out.println(rankingList);
-	}
+		StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+		SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
 
-	private static List<Ranking> getRankingList( ) {
-		List<Ranking> rankingList = new ArrayList<>();
-		for (int i = 1990; i < 2000; i++){
-			rankingList.add(new Ranking("Population", i, getRankingMap()));
-			rankingList.add(new Ranking("AverageLife", i, getRankingMap()));
-			rankingList.add(new Ranking("Area", i, getRankingMap()));
-		}
-		return rankingList;
-	}
+		CountryDetails countryDetails = new CountryDetails();
+		countryDetails.setInformation(InformationType.POPULATION);
+		countryDetails.setValue(40000000);
+		countryDetails.setYear(2017);
 
-	private static Map<Country, Integer> getRankingMap() {
-		Map<Country, Integer> rankingMap = new HashMap<>();
-		rankingMap.put(new Country("Ukraine"), new Random().nextInt(Integer.MAX_VALUE));
-		rankingMap.put(new Country("Turkey"), new Random().nextInt(Integer.MAX_VALUE));
-		rankingMap.put(new Country("England"), new Random().nextInt(Integer.MAX_VALUE));
-		return rankingMap;
+		CountryDetails countryDetails1 = new CountryDetails();
+		countryDetails1.setInformation(InformationType.AREA);
+		countryDetails1.setValue(191919);
+		countryDetails.setYear(2017);
+
+		Country country = new Country();
+		country.setName("Ukraine");
+		List<CountryDetails> list = new ArrayList<>();
+		list.add(countryDetails);
+		list.add(countryDetails1);
+		country.setCountryDetailsList(list);
+
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.persist(country);
+
+		Country country1 = session.get(Country.class,1L);
+		System.out.println(country1);
+		session.getTransaction().commit();
+
+		session.close();
+		sessionFactory.close();
 	}
 }
